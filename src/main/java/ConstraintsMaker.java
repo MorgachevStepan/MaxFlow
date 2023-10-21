223c0a7ee36e25df87cb3359b488f952f3d9560f
Stepan Morgachev <stepan.morgachev@gmail.com>
2023-10-21 19:03:40 +0300

    Wrote full lp file: objective function, compositive constraints, simple constraints

---
+++ src/main/java/ConstraintsMaker.java
@@ -0,0 +1,82 @@
+import org.jgrapht.Graph;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Stepan Morgachev
+ * @date 21.10.2023 18:22
+ */
+public class ConstraintsMaker {
+    private final BufferedWriter writer;
+    private final Graph<String, MyWeightedEdge> graph;
+    private final List<String> vertexWithSource;
+    private final List<String> vertexWithDistant;
+
+
+    public ConstraintsMaker(BufferedWriter writer, Graph<String, MyWeightedEdge> graph) {
+        this.writer = writer;
+        this.graph = graph;
+        vertexWithSource = new ArrayList<>();
+        vertexWithDistant = new ArrayList<>();
+        makeAdjacencyArrays();
+    }
+
+    private void makeAdjacencyArrays() {
+        for(MyWeightedEdge myWeightedEdge: graph.edgesOf(GraphCreator.SOURCE)){ //ищем вершины смежные с SOURCE
+            vertexWithSource.add(graph.getEdgeTarget(myWeightedEdge));
+        }
+        for (MyWeightedEdge myWeightedEdge: graph.edgesOf(GraphCreator.DISTANT)){ //ищем вершины смежные с DISTANT
+            vertexWithDistant.add(graph.getEdgeSource(myWeightedEdge));
+        }
+    }
+
+    public void makeObjectiveFunction() throws IOException {
+        writer.write("max: ");
+        for (MyWeightedEdge edge : graph.edgesOf(GraphCreator.SOURCE)) {
+            writer.write("+X_" + GraphCreator.SOURCE + "_" + graph.getEdgeTarget(edge));
+        }
+        writer.write(";\n\n");
+    }
+
+    public void makeSimpleConstraints() throws IOException {
+        for(String vertex: graph.vertexSet()){
+            for(MyWeightedEdge edge: graph.edgesOf(vertex)){
+                if(!vertex.equals(graph.getEdgeTarget(edge))) {
+                    writer.write("X_" + vertex + "_" + graph.getEdgeTarget(edge) + "<=" + edge.toString() + ";\n");
+                }
+            }
+        }
+    }
+
+    public void makeCompositeConstraints() throws IOException {
+        StringBuilder result = null;
+        for(String vertex: vertexWithSource){
+            result = new StringBuilder("X_" + GraphCreator.SOURCE + "_" + vertex + "=");
+            for(MyWeightedEdge myWeightedEdge: graph.edgesOf(vertex)){
+                if(!graph.getEdgeTarget(myWeightedEdge).equals(vertex)) {
+                    result.append("+X_").append(vertex).append("_").append(graph.getEdgeTarget(myWeightedEdge)); //создаю
+                }                                                                                                //первый ряд ограничений
+            }
+            result.append(";\n");
+            String buffer = result.toString();
+            writer.write(buffer.replace("=+", "="));
+        }
+        writer.write("\n");
+
+        for(String vertex: vertexWithDistant){
+            result = new StringBuilder("X_" + vertex + "_" + GraphCreator.DISTANT + "=");
+            for (MyWeightedEdge myWeightedEdge: graph.edgesOf(vertex)){
+                if(!graph.getEdgeSource(myWeightedEdge).equals(vertex)){
+                    result.append("+X_").append(graph.getEdgeSource(myWeightedEdge)).append("_").append(vertex); //создаю
+                }                                                                                                //второй ряж ограничений
+            }
+            result.append(";\n");
+            String buffer = result.toString();
+            writer.write(buffer.replace("=+", "="));
+        }
+        writer.write("\n");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,19 +19,10 @@ public class Main {
 
 
         try(BufferedWriter writer = new BufferedWriter(new FileWriter("graph.lp"))) {
-            writer.write("max: ");
-            for (MyWeightedEdge edge : graph.edgesOf(GraphCreator.SOURCE)) {
-                writer.write("+" + edge.toString() + "X_" + GraphCreator.SOURCE + "_" + graph.getEdgeTarget(edge));
-            }
-            writer.write(";\n\n");
-
-            for(String vertex: graph.vertexSet()){
-                for(MyWeightedEdge edge: graph.edgesOf(vertex)){
-                    if(vertex.equals(graph.getEdgeSource(edge))) {
-                        writer.write("X_" + vertex + "_" + graph.getEdgeTarget(edge) + "<=" + edge.toString() + ";\n");
-                    }
-                }
-            }
+            ConstraintsMaker constraintsMaker = new ConstraintsMaker(writer, graph);
+            constraintsMaker.makeObjectiveFunction();
+            constraintsMaker.makeCompositeConstraints();
+            constraintsMaker.makeSimpleConstraints();
         }catch (IOException e){
             System.err.println(e.getMessage());
         }
